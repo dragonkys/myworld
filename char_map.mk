a4ae4135be63b2043eaa5da7e8e962e8a57088c2
dragon <dragonkys@naver.com>
2018-10-06 12:10:42 +0900

    char_map 테스트하는 프로그램 추가 demo1.mk 는 일부 오류 내용 수정

--- char_map.mk
+++ char_map.mk
@@ -1,12 +1,12 @@
-OBJ=demo1.o
-PCSRC=demo1.pc
-SRC=demo1.c
+OBJ=char_map.o
+PCSRC=char_map.pc
+SRC=char_map.c
 INC=/usr/include/oracle/11.2/client64
 CC=gcc
 PROC=proc
 LDLIB=/usr/lib/oracle/11.2/client64/lib/
 FLAGS=-ldl -lm -lclntsh
-TARGET=demo1
+TARGET=char_map
 
 $(TARGET) : $(OBJ)
 	$(CC) -o $(TARGET) $(OBJ) -L$(LDLIB) $(FLAGS)
